d787bf2c471f26b31acc131c600bc5a9d8fb7835
chloech314 <chloe.uoft314@gmail.com>
2026-08-31 21:50:38 -0400

    Added DK strafing + remapped rotation
    1. Added raycasts to DK to support strafing
    2. WASD to walk. Q and E to turn 90 degrees / Left joystick to walk and RB and Lb to turn 90 degrees
    3. You can walk continuously forward by holding a cardinal key / holding the joystick in a direction but must press everytime you rotate (you cannot spin right round baby right round like a record baby right round round round by holding the rotate key)

--- features/player/dreadnought_killer/dreadnought_killer.gd
+++ features/player/dreadnought_killer/dreadnought_killer.gd
@@ -6,6 +6,8 @@ extends CharacterBody3D
 
 @onready var front_raycast: RayCast3D = $FrontRaycast
 @onready var back_raycast: RayCast3D = $BackRaycast
+@onready var right_raycast: RayCast3D = $RightRaycast
+@onready var left_raycast: RayCast3D = $LeftRaycast
 
 var grid_position: Vector2i = Vector2i.ZERO
 var _moving: bool = false
@@ -16,19 +18,25 @@ func _ready() -> void:
 	
 	front_raycast.target_position = Vector3(0, -cell_size, 0)
 	back_raycast.target_position = Vector3(0, cell_size, 0)
+	right_raycast.target_position = Vector3(0, cell_size , 0)
+	left_raycast.target_position = Vector3(0, cell_size, 0)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if _rotating or _moving:
-		return
-
-	if event.is_action_pressed("grid_rotate_left"):
-		rotate_left()
-	elif event.is_action_pressed("grid_rotate_right"):
-		rotate_right()
-	elif event.is_action_pressed("grid_move_forward"):
-		move_forward()
-	elif event.is_action_pressed("grid_move_back"):
-		move_backward()
+func _physics_process(_delta):
+	if not _moving:
+		if Input.is_action_pressed("move_forward"):
+			move_forward()
+		elif Input.is_action_pressed("move_back"):
+			move_backward()
+		elif Input.is_action_pressed("move_right"):
+			move_right()
+		elif Input.is_action_pressed("move_left"):
+			move_left()
+			
+	if not _rotating:
+		if Input.is_action_just_pressed("grid_rotate_left"):
+			rotate_left()
+		elif Input.is_action_just_pressed("grid_rotate_right"):
+			rotate_right()
 
 func rotate_left() -> void:
 	rotate_90_degrees(1)
@@ -37,37 +45,50 @@ func rotate_right() -> void:
 	rotate_90_degrees(-1)
 
 func move_forward() -> void:
-	var direction := get_forward_grid_direction()
+	var direction := get_grid_direction(-global_transform.basis.z)
 	
-	if !can_move_forward():
+	if !can_move(front_raycast):
 		bump_into_obstacle(direction)
 		return
 
 	try_move(direction)
 
 func move_backward() -> void:
-	var direction := -get_forward_grid_direction()
+	var direction := get_grid_direction(global_transform.basis.z)
 	
-	if !can_move_backwards():
+	if !can_move(back_raycast):
+		bump_into_obstacle(direction)
+		return
+		
+	try_move(direction)
+
+func move_right() -> void:
+	var direction := get_grid_direction(global_transform.basis.x)
+	
+	if !can_move(right_raycast):
+		bump_into_obstacle(direction)
+		return
+		
+	try_move(direction)
+	
+	
+func move_left() -> void:
+	var direction := get_grid_direction(-global_transform.basis.x)
+	
+	if !can_move(left_raycast):
 		bump_into_obstacle(direction)
 		return
 		
 	try_move(direction)
 
 # Movement
-func get_forward_grid_direction() -> Vector2i:
-	var forward := -global_transform.basis.z
+func get_grid_direction(local_direction: Vector3) -> Vector2i:
+	var dir := local_direction.normalized()
 
-	if abs(forward.x) > abs(forward.z):
-		return Vector2i(
-			signi(roundi(forward.x)),
-			0
-		)
+	if abs(dir.x) > abs(dir.z):
+		return Vector2i(signi(roundi(dir.x)), 0)
 	else:
-		return Vector2i(
-			0,
-			signi(roundi(forward.z))
-		)
+		return Vector2i(0, signi(roundi(dir.z)))
 
 func try_move(direction: Vector2i) -> void:
 	var target_grid_position := grid_position + direction
@@ -98,12 +119,9 @@ func grid_to_world(grid: Vector2i) -> Vector3:
 		grid.y * cell_size
 	)
 
-func can_move_forward() -> bool:
-	return !front_raycast.is_colliding()
+func can_move(raycast: RayCast3D) -> bool:
+	return !raycast.is_colliding()
 	
-func can_move_backwards() -> bool:
-	return !back_raycast.is_colliding()
-
 func bump_into_obstacle(
 	direction: Vector2i, 
 	distance: float = 0.25,
@@ -137,7 +155,7 @@ func rotate_90_degrees(direction: int) -> void:
 
 	var target_rotation = rotation.y + deg_to_rad(90.0 * direction)
 
-	var tween = create_tween()
+	var tween := create_tween()
 	tween.tween_property(
 		self,
 		"rotation:y",
